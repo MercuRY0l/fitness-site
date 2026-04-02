86dd31178a3c4f2a5696bd2cde16e79d74b3823e
MercuRY0l <udgitv@gmail.com>
2026-04-02 11:50:32 +0300

    Обновил модели бд

--- backend/database/models.py
+++ backend/database/models.py
@@ -10,24 +10,48 @@ class Base(DeclarativeBase):
 class Users(Base):
     __tablename__ = "users"
     
-    id : Mapped[int] = mapped_column(primary_key=True)
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
     login : Mapped[str] = mapped_column(String(255), nullable=False , unique=True)
     email : Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     password : Mapped[str] = mapped_column(String(255) , nullable=False)
     created_at : Mapped[datetime] = mapped_column(DateTime, nullable=False, default=func.now())
     
-    exercises : Mapped[list["Exercises"]] = relationship(back_populates="user")
+    workouts : Mapped[list["Workouts"]] = relationship("Workouts", back_populates="user")
 
 class Exercises(Base):
     
     __tablename__ = "exercises"
     
-    id : Mapped[int] = mapped_column(primary_key=True)
-    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
-    date : Mapped[datetime] = mapped_column()
-    approaches : Mapped[int] = mapped_column()
-    repetition : Mapped[int] = mapped_column()
-    exercise : Mapped[str] = mapped_column()
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    title : Mapped[str] = mapped_column(String(255), nullable=False , unique=True)
+    description : Mapped[str] = mapped_column(String(512), nullable=False)
+    difficulty : Mapped[int] = mapped_column(Integer(10), nullable=False)
+    image : Mapped[str] = mapped_column(String(255))
     
-    user: Mapped["Users"] = relationship(back_populates="exercises")
+    workouts : Mapped[list["Workout_Exercises"]] = relationship("Workouts", back_populates="exercise")
+
+class Workouts(Base):
+    
+    __tablename__ = "workouts"
+    
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id : Mapped[int] = mapped_column(nullable=False)
+    title : Mapped[str] = mapped_column(String(255), nullable=False)
+    date : Mapped[int] = mapped_column(DateTime, nullable=False)
+    created_at : Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    
+    user : Mapped[list["Users"]] = relationship("Users", back_populates="workouts")
+    exercises : Mapped[list["Workout_Exercises"]] = relationship("Exercises", back_populates="workout")
+    
+class Workout_Exercises(Base):
+    __tablename__ = "workout_exercises"
+    
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    workout_id : Mapped[int] = mapped_column(Integer, ForeignKey("workouts.id"))
+    exercise_id : Mapped[int] = mapped_column(Integer, ForeignKey("exercises.id"))
+    
+    workout = Mapped[list["Workout_Exercises"]] = relationship("Workouts", back_populates="exercises")
+    exercise : Mapped[list["Workout_Exercises"]] = relationship("Exercises", back_populates="workouts")
+    
+    
     
