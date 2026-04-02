2a6ac0d946cc215bdfcc71bff6d5a90efbd6cf89
MercuRY0l <udgitv@gmail.com>
2026-04-02 12:33:03 +0300

    Добавлены новые репозитории

---
+++ backend/database/repositories/workout_repo.py
@@ -0,0 +1,30 @@
+
+from ..connect import async_session
+from ..models import Workouts
+from sqlalchemy import select, delete
+from datetime import datetime
+
+class WorkoutRepository:
+    
+    async def find_workout_by_id(self, workout_id : int) -> Workouts:
+        async with async_session() as session:
+            stmt = select(Workouts).where(Workouts.id == workout_id)
+            res = await session.execute(stmt)
+            return res.scalars().one_or_none()
+        
+    
+    async def create_workout(self, user_id : int, title : str, date : datetime, created_at : datetime):
+        async with async_session() as session:
+            ex = Workouts(user_id=user_id, title=title, date=date, created_at=created_at)
+            session.add(ex)
+            await session.commit()
+            await session.refresh(ex)
+            return ex
+    
+    async def delete_workout_by_workout_id(self, workout_id : int):
+        async with async_session() as session:
+            stmt = delete(Workouts).where(Workouts.id == workout_id)
+            res = await session.execute(stmt)
+            await session.commit()      
+            return res.rowcount
+                 
