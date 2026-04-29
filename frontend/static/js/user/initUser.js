5dd8270a26d0a1def86c00fbb10d5323ca243ecb
MercuRY0l <udgitv@gmail.com>
2026-04-29 09:21:57 +0300

    Исправил баг, при нажатии на логин пользователя открывается окно авторизации

--- frontend/static/js/user/initUser.js
+++ frontend/static/js/user/initUser.js
@@ -3,19 +3,26 @@ import { API_URL } from "../config.js";
 import { apiFetch } from "../auth/apiFetch.js";
 
 export async function initUserPanel() {
-    const btn = document.getElementById("open-modal-user-btn");
-    if (!btn) return;
+    const loginBtn = document.getElementById("open-modal-user-btn");
+    const userPanel = document.getElementById("user-panel");
+
+    if (!loginBtn || !userPanel) return;
 
     const user = await checkAuth();
 
+    
     if (!user) {
-        btn.innerHTML = `Личный кабинет`;
+        loginBtn.style.display = "block";
+        userPanel.innerHTML = "";
         return;
     }
 
-    btn.innerHTML = `
+    loginBtn.style.display = "none";
+
+    userPanel.innerHTML = `
         <div class="user-menu-wrapper">
             <span class="user-name">${user.username}</span>
+
             <div class="user-dropdown">
                 <a href="/user/profile">Мой профиль</a>
                 <a href="/user/settings">Настройки</a>
@@ -28,7 +35,6 @@ export async function initUserPanel() {
 
     logoutBtn?.addEventListener("click", async (e) => {
         e.preventDefault();
-        e.stopPropagation();
 
         await apiFetch(`${API_URL}/auth/logout`, {
             method: "POST",
